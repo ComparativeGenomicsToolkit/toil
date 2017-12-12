5cf0ee4b1001c57f1fa1875fe89f72007d6a1593
Joel Armstrong <jcarmstr@ucsc.edu>
2017-12-12 13:56:47 -0800

    Always round up # of cores when submitting LSF jobs
    Previously, core values of, say, 0.1 would get rounded down to 0, which
    LSF doesn't support.
    
    Fixes BD2KGenomics/toil#1988.

--- src/toil/batchSystems/lsf.py
+++ src/toil/batchSystems/lsf.py
@@ -23,6 +23,7 @@ from builtins import str
 from builtins import range
 from past.utils import old_div
 import logging
+import math
 import subprocess
 import time
 from threading import Thread
@@ -60,7 +61,7 @@ def prepareBsub(cpu, mem):
             'rusage[mem=' + str(mem_resource) + ']" -M' + str(mem_limit)
     else:
         bsubMem = ''
-    cpuStr = '' if cpu is None else '-n ' + str(int(cpu))
+    cpuStr = '' if cpu is None else '-n ' + str(int(math.ceil(cpu)))
     bsubline = ["bsub", bsubMem, cpuStr,"-cwd", ".", "-o", "/dev/null", "-e",
         "/dev/null"]
     lsfArgs = os.getenv('TOIL_LSF_ARGS')
