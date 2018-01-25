1bc029209e3fc9e2e77fcddddc61c35cf320acf1
Joel Armstrong <jcarmstr@ucsc.edu>
2018-01-25 15:58:14 -0800

    Avoid dill/pathos version conflict

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def runSetup():
         license="Apache License v2.0",
         install_requires=[
             'bd2k-python-lib>=1.14a1.dev35',
-            'dill==0.2.5',
+            'dill==0.2.7',
             'six>=1.10.0',
             'future',
             'requests',
